dde4de393632872cfa7e0d1f82f6efe1a8a99db6
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-30 11:18:35 -0700

    Update files for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# v0.1.2
+* Update minimum constraint on batali dependency
+
 # v0.1.0
 * Initial release
--- lib/batali-tk/version.rb
+++ lib/batali-tk/version.rb
@@ -1,6 +1,6 @@
 module BataliTk
   # Library version
-  VERSION = Gem::Version.new('0.1.1')
+  VERSION = Gem::Version.new('0.1.2')
   # Constraint for test-kitchen dependency
   TK_CONSTRAINT = '~> 1.3'
 end
