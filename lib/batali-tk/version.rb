b0ab020cfe14b9e48c78b2f2e5ce5c45134407ff
Chris Roberts <code@chrisroberts.org>
2015-06-30 13:51:44 -0700

    Update version for development 0.1.5

--- lib/batali-tk/version.rb
+++ lib/batali-tk/version.rb
@@ -1,6 +1,6 @@
 module BataliTk
   # Library version
-  VERSION = Gem::Version.new('0.1.4')
+  VERSION = Gem::Version.new('0.1.5')
   # Constraint for test-kitchen dependency
   TK_CONSTRAINT = '~> 1.3'
 end
